f99003bbe6f0c88f8735e69dc8309e00690b1d53
Brian Fisher <tbfisher@gmail.com>
2018-12-17 08:53:05 -0700

    make response buffer sizes configurable

--- dev/conf/docker-entrypoint.sh
+++ dev/conf/docker-entrypoint.sh
@@ -6,6 +6,10 @@ echo "Configured NGINX docroot to ${WEB_DOCROOT}."
 
 sed -i 's@%WEB_PHPFPM%@'"${WEB_PHPFPM}"'@' /etc/nginx/conf.d/*.conf
 echo "Configured PHP FPM to ${WEB_PHPFPM}."
+sed -i 's@%WEB_FASTCGI_BUFFERS%@'"${WEB_FASTCGI_BUFFERS}"'@' /etc/nginx/conf.d/*.conf
+echo "Configured PHP FPM to ${WEB_FASTCGI_BUFFERS}."
+sed -i 's@%WEB_FASTCGI_BUFFER_SIZE%@'"${WEB_FASTCGI_BUFFER_SIZE}"'@' /etc/nginx/conf.d/*.conf
+echo "Configured PHP FPM to ${WEB_FASTCGI_BUFFER_SIZE}."
 
 sed -i 's@%WEB_DRUPAL_PRIVATE_FILES%@'"${WEB_DRUPAL_PRIVATE_FILES}"'@' /etc/nginx/conf.d/*.conf
 echo "Configured Drupal private files to ${WEB_DRUPAL_PRIVATE_FILES}."
